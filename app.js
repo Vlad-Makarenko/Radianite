e7f45c02a6a9c30df9d3658c0fe0cb2082062a89
Vlad-Makarenko <Makrenkovladok@gmail.com>
2022-05-30 18:06:37 +0300

    sample sockets added

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
+const  http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
+const server = http.createServer(app);
 dotenv.config({ path: "./.env" });
+
+
 
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || 'localhost';;
 
+app.use(cors());
 app.use(express.json({ extended: true }));
 app.use('/api/card', require('./routes/card.routes'))
 app.use('/api/auth', require('./routes/auth.routes'))
@@ -15,7 +22,26 @@ app.use('/api/user', require('./routes/user.routes'))
 
 
 
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    method: [ 'GET', 'POST' ]
+  }
+});
 
-app.listen(PORT, () =>
+io.on('connection', (socket) => {
+  console.log(socket.id)
+
+  socket.on('Join_Room', (room) => {
+    socket.join(room)
+  })
+
+  socket.on('Send_Message', (data) => {
+    console.log(data)
+    socket.to(data.room).emit("recieved_message", data.message)
+  })
+})
+
+server.listen(PORT, () =>
   console.log(`server is running on http://${HOST}:${PORT}`)
 );
--- client/src/pages/Rules.jsx
+++ client/src/pages/Rules.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client"
+const socket = io('http://localhost:5000')
 
 
 export const Rules = () => {
+  const [room, setRoom] = useState('')
+  const [message, setMessage] = useState('');
+  const [recivedMessage, setRecivedMessage] = useState('');
+
+
+  const sendMessage = () => {
+    socket.emit('Send_Message', {message, room})
+  }
+
+  const joinRoom = () => {
+    if(room !== ''){
+      socket.emit('Join_Room', room)
+    }
+  }
+
+  useEffect( ()=> {
+    socket.on('recieved_message', (data) => {
+      setRecivedMessage(data);
+    } )
+  }, [])
 
   return (
       <div>
         <h1>Rules</h1>
+        <input placeholder="Message..." onChange={ (event) => {
+          setRoom(event.target.value)
+        } }/>
+        <button onClick={joinRoom}>Join Room</button>
+        <input placeholder="Message..." onChange={ (event) => {
+          setMessage(event.target.value)
+        } }/>
+        <button onClick={sendMessage}>Send Message</button>
+        <h1>Message: {recivedMessage} </h1>
       </div>
   );
 };
