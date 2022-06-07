290426e9772053d4570f3c6e3097be6406b4ab02
Vlad-Makarenko <Makrenkovladok@gmail.com>
2022-06-07 22:03:43 +0300

    info block added

---
+++ client/src/components/InfoBlock.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import "../styles/InfoBlock.css";
+
+export const InfoBlock = ({
+  oppTimer = "00:00",
+  turn,
+  userTimer = "00:00",
+  action,
+}) => {
+  return (
+    <div className="InfoBlock">
+      <div className="col s12 OppTimer">
+        <span>{oppTimer}</span>
+      </div>
+      <div className="col s12 TurnBlock">
+
+          <button
+            className="btn waves-effect waves-light orange"
+            onClick={action}
+            disabled={!turn}
+          >
+            End turn
+          </button>
+        </div>
+        <div className="col s12 TextInfBlock">
+          {turn ? (
+            <span className="TurnText" style={{color: 'greenyellow'}}>Your turn</span>
+          ) : (
+            <span className="TurnText" style={{color: 'red'}}>Opponent turn</span>
+          )}
+        </div>
+
+      <div className="col s12 UserTimer">
+        <span>{userTimer}</span>
+      </div>
+    </div>
+  );
+};
--- client/src/pages/Gameplay.jsx
+++ client/src/pages/Gameplay.jsx
@@ -1,52 +1,41 @@
 import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import io from "socket.io-client";
+
 import { CardList } from "../components/CardList";
 import { CardPopup } from "../components/CardPopup";
 import { Loader } from "../components/Loader";
 import { useBattle } from "../hooks/battle.hook";
 import { BattleContext } from "../contexts/BattleContext";
 import { Table } from "../components/Table";
-import { useParams } from "react-router-dom";
-import Game from "../Game/Game";
-import Player from "../Game/Player";
-
-import "../styles/Gameplay.css";
-
-import io from "socket.io-client";
 import { AuthContext } from "../contexts/AuthContext";
 import { BattleProfile } from "../components/BattleProfile";
 import { useBattleProfile } from "../hooks/battleProfile.hook";
+
+import Game from "../Game/Game";
+import Player from "../Game/Player";
+import { InfoBlock } from "../components/InfoBlock";
+
 const socket = io("http://localhost:5000");
 
 function noop() {}
 
-function changeTurn() {
-  socket.emit("changeTurn");
-}
-
 export const Gameplay = () => {
+  const { userId } = useContext(AuthContext);
+
   const [popupCard, setPopupCard] = useState(null); // TODO: обнулять взависимости от счетчика карт
   const [waiting, setWaiting] = useState(true);
   const [turn, setTurn] = useState(false);
 
-  const { userId } = useContext(AuthContext);
   const battleInfo = useBattleProfile();
-
-  // const {
-  //   userCards,
-  //   setUserCards,
-  //   oppCards,
-  //   setOppCards,
-  //   tableUserCards,
-  //   setTableUserCards,
-  //   tableOppCards,
-  //   setTableOppCards,
-  //   moveCard,
-  // } = useBattle();
-
   const state = useBattle();
   state.socket = socket;
 
   const room = useParams().id;
+
+  const changeTurn = () => {
+    socket.emit("changeTurn");
+  };
 
   useEffect(() => {
     socket.emit("initGame", { room, userId });
@@ -56,12 +45,11 @@ export const Gameplay = () => {
     socket.on("waiting", (data) => {
       setWaiting(true);
     });
+
     socket.on("startGame", (data) => {
-      // battleInfo.setOpponentLogin(data.opponent)
-      // battleInfo.setUserLogin(data.player)
       const player = new Player(data.player);
       const opponent = new Player(data.opponent);
-      const game = new Game(player, socket, opponent, {
+      new Game(player, socket, opponent, {
         ...state,
         ...battleInfo,
       });
@@ -69,34 +57,18 @@ export const Gameplay = () => {
       console.log(data.turn);
       setWaiting(false);
     });
-  }, [state, battleInfo]);
 
-  useEffect(() => {
     socket.on("changeTurn", (data) => {
       setTurn(data.turn);
     });
-  }, []);
+  }, [state, battleInfo]);
 
   if (waiting) {
     return <Loader info={"Waiting for the opponent..."} />;
   }
 
   return (
-    <BattleContext.Provider
-      // value={{
-      //   userCards,
-      //   setUserCards,
-      //   oppCards,
-      //   setOppCards,
-      //   tableUserCards,
-      //   setTableUserCards,
-      //   tableOppCards,
-      //   setTableOppCards,
-      //   moveCard,
-      // }}
-      value={state}
-    >
-      {/* {number == 1 && <div className="row pipec"> */}
+    <BattleContext.Provider value={state}>
       <div className="row">
         <div className="col s6 offset-s3">
           <CardList
@@ -120,17 +92,11 @@ export const Gameplay = () => {
           <Table setPopupCard={setPopupCard} />
         </div>
         <div className="col s3">
-          <button
-            className="btn waves-effect waves-light changeDescription"
-            name="action"
-            onClick={changeTurn} //? ()??
-          >
-            End turn
-          </button>
+          <InfoBlock oppTimer="00:10" userTimer="00:10" action={changeTurn} turn={turn} />
         </div>
+        
         <div className="col s6 offset-s3">
           <CardList
-            // classes={["CardList"]}
             classes={turn ? ["CardList"] : ["CardList", "disabledTurn"]}
             side="User"
             cards={state.userCards}
